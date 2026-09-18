46e525719ff680b4382b45197884b90836b1d634
mansoor02-dev <mansoorabid0202@gmail.com>
2026-09-18 01:45:07 -0400

    Fixed giftRoutes

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
         const collection = db.collection("gifts");
 
         // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-        const gifts = collection.findMany({});
+        const gifts = await collection.find({}).toArray();
 
         // Task 4: return the gifts using the res.json method
         res.json(gifts);
@@ -24,7 +24,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
-        const db = connectToDatabase();
+        const db = await connectToDatabase();
 
         // Task 2: use the collection() method to retrieve the gift collection
         const collection = db.collection("gifts");
@@ -32,7 +32,7 @@ router.get('/:id', async (req, res) => {
         const id = req.params.id;
 
         // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
-        gift = collection.findOne({"id": id});
+        gift = await collection.findOne({id: id});
 
         if (!gift) {
             return res.status(404).send('Gift not found');
