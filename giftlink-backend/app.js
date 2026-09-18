0adf1dbe43d34454e44c9b2dd57f5cd134ecd31f
mansoor02-dev <mansoorabid0202@gmail.com>
2026-09-18 01:21:45 -0400

    Add serviceRoute code

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -23,10 +23,9 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+const searchRoutes = require('./routes/searchRoutes')
 
 
 const pinoHttp = require('pino-http');
@@ -39,8 +38,7 @@ app.use(pinoHttp({ logger }));
 //{{insert code here}}
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
-
+app.use('/api/search',searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
