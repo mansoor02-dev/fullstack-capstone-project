454bfd8891561e915558ecbcb018a44d50e1bf10
mansoor02-dev <mansoorabid0202@gmail.com>
2026-09-19 12:25:40 -0400

    Add update route in the backend

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -15,7 +15,7 @@ router.post('/register', async (req, res) => {
     try {
         const db = await connectToDatabase();
         const collection = db.collection('users');
-        const existingEmail = await collection.findOne({email: req.body.email});
+        const existingEmail = await collection.findOne({ email: req.body.email });
 
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
@@ -36,7 +36,7 @@ router.post('/register', async (req, res) => {
         const authtoken = jwt.sign(payload, JWT_SECRET);
 
         logger.info('User registered successfully');
-        res.json({authtoken,email});
+        res.json({ authtoken, email });
     } catch (e) {
         return res.status(500).send('Internal server error');
     }
@@ -46,13 +46,13 @@ router.post('/login', async (req, res) => {
     try {
         const db = await connectToDatabase();
         const collection = db.collection('users');
-		const theUser = await collection.findOne({email: req.body.email});
+        const theUser = await collection.findOne({ email: req.body.email });
 
         if (theUser) {
             let result = await bcryptjs.compare(req.body.password, theUser.password);
             if (!result) {
                 logger.error('Passwords do not match');
-        		return res.status(404).json({ error: 'Wrong pasword' });
+                return res.status(404).json({ error: 'Wrong pasword' });
             }
             const userName = theUser.firstName;
             const userEmail = theUser.email;
@@ -63,14 +63,63 @@ router.post('/login', async (req, res) => {
             };
             const authtoken = jwt.sign(payload, JWT_SECRET);
             logger.info('User logged in successfully');
-            res.json({authtoken, userName, userEmail });
+            res.json({ authtoken, userName, userEmail });
         } else {
             logger.error('User not found');
-    		return res.status(404).json({ error: 'User not found' });
+            return res.status(404).json({ error: 'User not found' });
         }
     } catch (e) {
-         return res.status(500).send('Internal server error');
+        return res.status(500).send('Internal server error');
 
+    }
+});
+
+router.put('/update', async (req, res) => {
+
+    const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const email = req.headers.email;
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+		}
+
+		const db = await connectToDatabase();
+        const collection = db.collection("users");
+
+        const existingUser = await collection.findOne({ email });
+
+        if (!existingUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        existingUser.firstName = req.body.name;
+        existingUser.updatedAt = new Date();
+
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+        );
+
+        const payload = {
+            user: {
+                id: updatedUser._id.toString(),
+            },
+        };
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        logger.info('User updated successfully');
+
+        res.json({ authtoken });
+    } catch (error) {
+        logger.error(error);
+        return res.status(500).send("Internal Server Error");
     }
 });
 
