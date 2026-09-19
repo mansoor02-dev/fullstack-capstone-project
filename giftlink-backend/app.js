c6d67b3b68a8239f0ebbdc6423dbf14da92b07aa
mansoor02-dev <mansoorabid0202@gmail.com>
2026-09-19 09:05:30 -0400

    Add registerationfunctionality

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,7 +7,6 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
-
 const app = express();
 app.use("*",cors());
 const port = 3060;
@@ -23,9 +22,8 @@ app.use(express.json());
 
 // Route files
 const giftRoutes = require('./routes/giftRoutes');
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes')
-
+const authRoutes = require('./routes/authRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -33,11 +31,9 @@ const logger = require('./logger');
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 app.use('/api/gifts', giftRoutes);
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search',searchRoutes);
+app.use('/api/auth', authRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
