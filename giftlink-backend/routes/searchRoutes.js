5919639cd7f6c6353ee8df427f61954c80a4084f
mansoor02-dev <mansoorabid0202@gmail.com>
2026-09-20 09:40:54 +0500

    Add semicolon in searchRoutes.js in backend

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -7,7 +7,7 @@ const connectToDatabase = require('../models/db');
 router.get('/', async (req, res, next) => {
     try {
         // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
-        const db = await connectToDatabase()
+        const db = await connectToDatabase();
 
         const collection = db.collection("gifts");
 
@@ -21,10 +21,10 @@ router.get('/', async (req, res, next) => {
 
         // Task 3: Add other filters to the query
         if (req.query.category) {
-            query.category = req.query.category
+            query.category = req.query.category;
         }
         if (req.query.condition) {
-            query.condition = req.query.condition
+            query.condition = req.query.condition;
         }
         if (req.query.age_years) {
             query.age_years = { $lte: parseInt(req.query.age_years) };
